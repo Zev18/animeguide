cffc32f18da9f47f8f2bc9b0bfc68809f4297057
zev18 <zevross@gmail.com>
2023-09-22 00:01:22 +0900

    Updated prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,17 @@
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  proseWrap: "always",
+};
+
 module.exports = {
+  config,
   plugins: ["prettier-plugin-tailwindcss"],
 };
