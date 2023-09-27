c528613b9d594ad2967006c29f8446141d8a07ee
zev18 <zevross@gmail.com>
2023-09-27 17:59:56 +0900

    Ok now it should be fixed fr

--- src/utils/supabaseServer.ts
+++ src/utils/supabaseServer.ts
@@ -1,7 +1,13 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { Database } from "../../database.types";
+import { createServer } from "http";
 
-export default await function supabaseServerComponentClient<Database>() {
-  return createServerComponentClient({ cookies });
-};
+export default async function supabaseServerComponentClient<Database>() {
+  const cookieStore = cookies();
+  return createServerComponentClient({ cookies: () => cookieStore });
+}
+
+// export await function supabaseServerComponentClient<Database>() {
+//   return createServerComponentClient({ cookies });
+// };
